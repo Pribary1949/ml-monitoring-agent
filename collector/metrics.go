579e76387de06a5286b40e81f761ffa7724f908b
Marcus J. Sterling <inaradarem@gmx.net>
2024-05-20 10:15:00 -0400

    Feature implementation: Part 2 of core ml-monitoring-agent modules

--- collector/metrics.go
+++ collector/metrics.go
@@ -14,3 +14,21 @@ type MetricsCollector struct {
 	driftScore    prometheus.Gauge
 	errorRate     prometheus.Summary
 }
+
+func NewMetricsCollector(namespace string) *MetricsCollector {
+	return &MetricsCollector{
+		inferenceTime: promauto.NewHistogram(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Name:      "inference_duration_seconds",
+			Help:      "Latency of model inference in seconds",
+			Buckets:   prometheus.DefBuckets,
+		}),
+		requestCount: promauto.NewCounter(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "total_requests",
+			Help:      "Total number of inference requests",
+		}),
+		driftScore: promauto.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "model_drift_score",
+			Help:      "Current calculated drift score (0-1)",
